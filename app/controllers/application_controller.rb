a16b083ea5c7e9dc1a63d94892c120437f0bd2a5
rachelkathleen <rachel.hawa@gmail.com>
2019-03-20 13:01:49 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get "/articles/new" do
+    @article = Article.create
+    erb :new
+  end
+
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+  #
+  # get '/articles/:id' do
+  #   #show info about a specific article
+  # end
+  #
+  # get '/articles/:id/edit' do
+  #   #show a form to edit an article
+  # end
+  #
+  # patch '/articles/:id' do
+  #   #edits an article in the database, then redirects
+  # end
+  #
+  # delete '/articles/:id'
+  #   #deletes an article in the database, then redirects
+  # end
+
 end
