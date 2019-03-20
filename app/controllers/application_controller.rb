44dbe1bfe2ba49074665939a50c8b49ab076f037
rachelkathleen <rachel.hawa@gmail.com>
2019-03-20 14:01:32 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,35 +12,45 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles"
   end
 
-  get "/articles" do
-    @articles = Article.all
-    erb :index
-  end
-
   get "/articles/new" do
-    @article = Article.create
     erb :new
   end
 
   post "/articles" do
     @article = Article.create(params)
-    redirect to "/articles/#{ @article.id }"
+    redirect "/articles/#{ @article.id }"
   end
-  #
-  # get '/articles/:id' do
-  #   #show info about a specific article
-  # end
-  #
-  # get '/articles/:id/edit' do
-  #   #show a form to edit an article
-  # end
-  #
-  # patch '/articles/:id' do
-  #   #edits an article in the database, then redirects
-  # end
-  #
-  # delete '/articles/:id'
-  #   #deletes an article in the database, then redirects
-  # end
 
+  get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+   @article = Article.find(params[:id])
+   @article.update(title: params[:title], content: params[:content])
+
+   redirect "/articles/#{@article.id}"
+  end
+
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    if @article.delete
+      redirect '/articles'
+    else
+      redirect "/articles/#{@article.id}"
+    end
+  end
 end
