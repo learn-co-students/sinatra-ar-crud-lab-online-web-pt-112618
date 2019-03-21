27479af4cd295652d44a834267b6ffc1ee742cab
Chris Philbin <chris@chrisphilbin.net>
2019-03-21 23:01:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles/new'
+    erb :new
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@
 class Article < ActiveRecord::Base
   attr_accessor :title, :content
   
-  def initialize(title, content)
-    @content = content
-    @title = title
-  end
+  # def initialize(title, content)
+  #   @content = content
+  #   @title = title
+  # end
 end
