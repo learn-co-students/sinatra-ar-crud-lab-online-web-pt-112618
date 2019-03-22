226ba6e2a473f6e685cedbf0121cb2c02826c288
Chris Philbin <chris@chrisphilbin.net>
2019-03-22 00:00:27 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  attr_accessor :title, :content
+  # attr_accessor :title, :content
   
   # def initialize(title, content)
   #   @content = content
   #   @title = title
   # end
+  
 end
