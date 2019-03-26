588a926299ac6e41c69cb81a2717fd17f7205817
github username <github email address>
2019-03-26 13:07:47 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -9,5 +10,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  get '/articles' do
+    #2. shows all articles with id
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do #1. create this route first to extract form data
+    @article = Article.new
+    erb :new
+  end
+
+  post '/articles' do #3. posts all created articles from new.erb
+    @article = Article.create(params)
+  end
+
 end
